b11e303cdc24d584ee985f48fec845340f072255
Corey Redding <coreyredding@Coreys-iMac.local>
2017-05-03 06:07:52 -0700

    create initial files and begin to coordinate back end and front end logic for user to input yearly dates

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//Business End Logic//
+var leapYear = function(year) {
+  return false;
+};
+
+
+//User Interface Logic//
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
